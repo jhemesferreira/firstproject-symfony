aa3d487dd0a931317e879d8dbcf18d24a31ec3fe
jhemes.ferreira <jhemes.ferreira.dla@gmail.com>
2020-11-03 10:23:01 +0100

    added: filter

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Property;
+use App\Entity\PropertySearch;
+use App\Form\PropertySearchType;
 use App\Repository\PropertyRepository;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\ORM\EntityManagerInterface;
@@ -39,16 +41,19 @@ class PropertyController extends AbstractController
   public function index(PaginatorInterface $paginator, Request $request): Response
   {
 
-
+	$search = new PropertySearch();
+	$form = $this->createForm(PropertySearchType::class, $search);
+	$form->handleRequest(($request));
 
     $properties = $paginator->paginate(
-      $this->repository->findAllVisibleQuery(), 
+      $this->repository->findAllVisibleQuery($search), 
       $request->query->getInt('page', 1), 10);
 
 
     return $this->render('pages/property/index.html.twig', [
       'current_menu' => 'property',
-      'properties' => $properties
+	  'properties'   => $properties,
+	  'form'         => $form->createView()
     ]);
   }
   /**
---
+++ src/Entity/PropertySearch.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class PropertySearch{
+
+	/**
+	 * @var int|null
+	 * @Assert\Range(min=10, max=1000)
+	 */
+	private $maxPrice; 
+
+	/**
+	 * @var int|null
+	 */
+	private $minSurface; 
+
+
+	public function getMaxPrice(): ?int
+	{
+		return $this->maxPrice;
+	}
+
+	public function setMaxPrice(int $maxPrice): PropertySearch 
+	{
+		$this->maxPrice = $maxPrice;
+		return $this;
+	}
+
+	public function getMinSurface(): ?int
+	{
+		return $this->minSurface;
+	}
+
+	public function setMinSurface(int $minSurface)
+	{
+		$this->minSurface = $minSurface;
+		return $this;
+	}
+}
